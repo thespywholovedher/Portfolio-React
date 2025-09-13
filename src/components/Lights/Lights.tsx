038e1f9b900dbc32bfe3f4a8bee74d768fe27823
Ahmal Jones <ahmal.jones@gmail.com>
2025-09-13 01:09:42 -0400

    button lights now light up

--- src/components/Lights/Lights.tsx
+++ src/components/Lights/Lights.tsx
@@ -7,30 +7,38 @@ type Light = {
   color: string
 }
 
-const lights:Light[] = [
+const lightsJson:Light[] = [
   { id: 1, on: false, color: "rgb(0,0,0)"},
-  { id: 2, on: false, color: "rgb(230,159,0)" },
+  { id: 2, on: true, color: "rgb(230,159,0)" },
   { id: 3, on: false, color: "rgb(86,180,233)"},
   { id: 4, on: false, color: "rgb(0,158,115)"},
   { id: 5, on: false, color: "rgb(240,228,66)"},
-  { id: 6, on: false, color: "rgb(0,114,178)" },
+  { id: 6, on: true, color: "rgb(0,114,178)" },
   { id: 7, on: false, color: "rgb(213,94,0)" },
   { id: 8, on: false, color: "rgb(204,121,167)"},
 ];
 
-const Light = (props:{id:number, on: boolean, color:string}) => {
+const Light = (props:{id:number, on: boolean, color:string, toggle: (id:number) => void }) => {
   const style = {
     backgroundColor:props.color,
     opacity: props.on ? 1 : .1
   };
   return (
-    <button key={props.id} style={style} className={"box"}>10</button>
+    <button onClick={() => props.toggle(props.id)} style={style} className={"box"}>10</button>
   )
 }
 
 const Lights = () => {
-  const lightElement = lights.map((light) => {
-    return <Light id={light.id} on={light.on} color={light.color} />
+  const [lights, setLights] = React.useState<Light[]>(lightsJson);
+
+  const toggle = (id:number):void => {
+    setLights((prevLights) =>{
+      return prevLights.map((light:Light):Light => light.id === id ? {...light, on:!light.on} : light)
+    })
+  }
+
+  const lightElement = lights.map((light:Light):React.JSX.Element => {
+    return <Light key={light.id} id={light.id} on={light.on} color={light.color} toggle={toggle}/>
   })
 
   return (
