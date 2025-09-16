188131ba5cfc9ea7a2ea978ae91a931643f85a44
Ahmal Jones <ahmal.jones@gmail.com>
2025-09-15 23:54:28 -0400

    Switch from flex to grid and can now render entire array

--- src/LightsOff/LightsOff.tsx
+++ src/LightsOff/LightsOff.tsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import './lightsOff.css'
 
+const generateGrid = <T,> (n:number) => {
+  let array:number[][] = 
+    Array.from({ length: n }, (_, i) => 
+      Array.from({ length: n}, (_, j) => i+j));
+
+  console.log(array)
+  return array;
+}
+
 const Grid = (props: React.PropsWithChildren) => {
     return (
      <div className='grid'>{props.children}</div>   
@@ -14,12 +23,21 @@ const Cell = () => {
 }
 
 const LightsOff = () => {
-  const cells = ([1,2,3,4,5]).map((e,i) => { return <Cell key={i} />}) 
+  const [grid, setGrid] = React.useState(generateGrid(5))
+
+
+  const gridElements:React.JSX.Element[] = []
+  for(let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      gridElements.push(<Cell key={i+j*10} />)
+    }
+  }
+  const cells = grid.flatMap((e,i) => { return <Cell key={i} />}) 
   return (
     <>
       <div>LightsOff</div>
       <Grid>
-        {cells}
+        {gridElements}
       </Grid>
     </>
   )
